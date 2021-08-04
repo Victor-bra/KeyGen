0fbf6d790b1874e13655c181ec2539a0033cdd13
Victor_bra_ <victorBra@gmail.com>
2021-08-04 19:54:19 +0300

    new project

---
+++ KeyGen/XtraForm1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace KeyGen
+{
+    partial class KeyGenForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel(this.components);
+            this.ColTextBox = new ns1.BunifuMaterialTextbox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.bunifuProgressBar1 = new ns1.BunifuProgressBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // defaultLookAndFeel1
+            // 
+            this.defaultLookAndFeel1.LookAndFeel.SkinName = "Office 2016 Dark";
+            // 
+            // ColTextBox
+            // 
+            this.ColTextBox.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.ColTextBox.Font = new System.Drawing.Font("Century Gothic", 9.75F);
+            this.ColTextBox.ForeColor = System.Drawing.Color.White;
+            this.ColTextBox.HintForeColor = System.Drawing.Color.Empty;
+            this.ColTextBox.HintText = "";
+            this.ColTextBox.isPassword = false;
+            this.ColTextBox.LineFocusedColor = System.Drawing.Color.Blue;
+            this.ColTextBox.LineIdleColor = System.Drawing.Color.Gray;
+            this.ColTextBox.LineMouseHoverColor = System.Drawing.Color.Blue;
+            this.ColTextBox.LineThickness = 3;
+            this.ColTextBox.Location = new System.Drawing.Point(13, 13);
+            this.ColTextBox.Margin = new System.Windows.Forms.Padding(4);
+            this.ColTextBox.Name = "ColTextBox";
+            this.ColTextBox.Size = new System.Drawing.Size(370, 27);
+            this.ColTextBox.TabIndex = 0;
+            this.ColTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(390, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Выполнить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(471, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Записать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(278, 47);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(259, 95);
+            this.listBox1.TabIndex = 6;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(13, 47);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(238, 95);
+            this.listBox2.TabIndex = 7;
+            // 
+            // bunifuProgressBar1
+            // 
+            this.bunifuProgressBar1.BackColor = System.Drawing.Color.Silver;
+            this.bunifuProgressBar1.BorderRadius = 5;
+            this.bunifuProgressBar1.Location = new System.Drawing.Point(27, 349);
+            this.bunifuProgressBar1.MaximumValue = 100;
+            this.bunifuProgressBar1.Name = "bunifuProgressBar1";
+            this.bunifuProgressBar1.ProgressColor = System.Drawing.Color.Teal;
+            this.bunifuProgressBar1.Size = new System.Drawing.Size(712, 23);
+            this.bunifuProgressBar1.TabIndex = 8;
+            this.bunifuProgressBar1.Value = 0;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(12, 148);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Создать БД";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 177);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Size = new System.Drawing.Size(370, 150);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Code1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Code2";
+            this.Column2.Name = "Column2";
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(390, 177);
+            this.dataGridView2.Name = "dataGridView2";
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.HotTrack;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.Black;
+            this.dataGridView2.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView2.Size = new System.Drawing.Size(349, 150);
+            this.dataGridView2.TabIndex = 11;
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(624, 17);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 12;
+            this.button4.Text = "отключится";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(543, 17);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Показать";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // KeyGenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 390);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.bunifuProgressBar1);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ColTextBox);
+            this.Name = "KeyGenForm";
+            this.Text = "KeyGen";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private ns1.BunifuMaterialTextbox ColTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private ns1.BunifuProgressBar bunifuProgressBar1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ KeyGen/XtraForm1.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using Bunifu;
+using System.Threading;
+using System.Data.OleDb;
+
+namespace KeyGen
+{
+    public partial class KeyGenForm : DevExpress.XtraEditors.XtraForm
+    {
+        
+        public Thread trd;
+        public int x = 0;
+        public static string ConnString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Activator_Cods.mdb;Jet OLEDB:Engine Type=5;Jet OLEDB:Database Password=Baranchik";
+        const String KEY_CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        public OleDbConnection baza;
+        public OleDbDataAdapter dA;
+        public DataSet ds;
+        public KeyGenForm()
+        {
+            InitializeComponent();
+            Application.DoEvents();
+            //Создание Access файла
+            ADOX.Catalog BD = new ADOX.Catalog();
+            try
+            {
+                BD.Create("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Activator_Cods.mdb;Jet OLEDB:Engine Type=5;Jet OLEDB:Database Password=Baranchik");
+                BD = null;
+            }
+            catch
+            {
+                System.IO.File.Delete("Activator_Cods.mdb");
+                BD.Create("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Activator_Cods.mdb;Jet OLEDB:Engine Type=5;Jet OLEDB:Database Password=Baranchik");
+                BD = null;
+            }
+            //Открытие Файла
+            baza = new OleDbConnection(ConnString);
+            baza.Open();
+            //Создание таблицы в БД
+            OleDbCommand com = new OleDbCommand("CREATE TABLE Actvators (Cod int IDENTITY(1,1),Code1 STRING, Code2 string)", baza);
+            com.ExecuteNonQuery();
+            MessageBox.Show("База созданна");
+            
+            //button3.Visible = false;
+        }
+        Random rnd = new Random();
+        string GenerateKey()
+        { 
+            string results = "";
+            for (int i = 0; i < 7; i++)
+            {
+                results += KEY_CHARS[rnd.Next(0, KEY_CHARS.Length)];
+            }
+            return results;
+        }
+        string GenerateKey2()
+        {
+            string results = "";
+            for (int i = 0; i < 35; i++)
+            {
+                results += KEY_CHARS[rnd.Next(0, KEY_CHARS.Length)];
+            }
+            return results;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bunifuProgressBar1.Value = 0;
+            bunifuProgressBar1.MaximumValue = x;
+            string query = "";
+            for (int i = 0; i < x; i++)
+            {
+                query = "INSERT INTO Actvators (Code1, Code2) VALUES ('" + dataGridView1.Rows[i].Cells[0].Value+"', '"+ dataGridView1.Rows[i].Cells[1].Value + "')";
+                // создаем объект OleDbCommand для выполнения запроса к БД MS Access
+                OleDbCommand command = new OleDbCommand(query, baza);
+                // выполняем запрос к MS Access
+                command.ExecuteNonQuery();
+                bunifuProgressBar1.Value++;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           try
+           {
+               x = Convert.ToInt32(ColTextBox.Text);
+           }
+           catch
+           {
+               MessageBox.Show("Не верный ввод");
+           }
+           bunifuProgressBar1.Value = 0;
+           bunifuProgressBar1.MaximumValue = x;
+            
+            for (int i = 0; i < x; i++)
+           {
+                listBox1.Items.Insert(i, GenerateKey()+"-"+ GenerateKey() + "-" + GenerateKey());
+                listBox2.Items.Insert(i, GenerateKey2());
+                dataGridView1.Rows.Add();
+                dataGridView1.Rows[i].Cells[0].Value = listBox2.Items[i];
+               dataGridView1.Rows[i].Cells[1].Value = listBox1.Items[i];
+               bunifuProgressBar1.Value++;
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            dA.Dispose();
+            ds.Dispose();
+            baza.Dispose();
+            baza = null;
+            ds = null;
+            dA = null;
+            Application.Exit();
+            MessageBox.Show("Отключено");
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string CmdText = "SELECT * FROM [Actvators]";
+            dA = new OleDbDataAdapter(CmdText, ConnString);
+            ds = new DataSet();
+            dA.Fill(ds, "[Actvators]");
+            dataGridView2.DataSource = ds.Tables[0].DefaultView;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+    }
+}
